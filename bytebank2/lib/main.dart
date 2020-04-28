2257b814cdc80160c8fa2501d8371164778d8299
geovannaSilva <aparecida.geovanna483@gmail.com>
2020-04-28 17:33:30 -0300

    Second Commit

--- bytebank2/lib/main.dart
+++ bytebank2/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebank2/components/transaction_auth_dialog.dart';
 import 'package:flutter/material.dart';
 import 'package:bytebank2/screens/dashboard.dart';
 
--- bytebank2/lib/webclients/transaction_webclient.dart
+++ bytebank2/lib/webclients/transaction_webclient.dart
@@ -16,12 +16,12 @@ class TransactionWebClient {
   }
 
 
-  Future<Transaction> save(Transaction transaction) async {
+  Future<Transaction> save(Transaction transaction, String password) async {
     final String transactionJson = jsonEncode(transaction.toJson());
 
     final Response response = await client.post(baseUrl, headers: {
       'Content-type' : 'application/json',
-      'password' : '1000'
+      'password' : password,
     }, body: transactionJson);
 
     return Transaction.fromJson(jsonDecode(response.body));
