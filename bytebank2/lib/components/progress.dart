c275576e004a0a36bbfce8e27b823deb807c745d
geovannaSilva <aparecida.geovanna483@gmail.com>
2020-04-28 14:58:43 -0300

    First Commit

---
+++ bytebank2/lib/components/progress.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Progress extends StatelessWidget{
+
+  final String message;
+
+  Progress({this.message = 'Loading'});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return  Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          CircularProgressIndicator(),
+          Text(message),
+        ],
+      ),
+    );
+  }
+
+}
