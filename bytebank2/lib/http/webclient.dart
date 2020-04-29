4cd7aa0da88be812ffd02d88c2e68705c74a3aee
geovannaSilva <aparecida.geovanna483@gmail.com>
2020-04-29 20:09:56 -0300

    Quarto Commit

--- bytebank2/lib/http/webclient.dart
+++ bytebank2/lib/http/webclient.dart
@@ -4,8 +4,10 @@ import 'package:http_interceptor/http_interceptor.dart';
 
 
 
-final Client client =
-    HttpClientWithInterceptor.build(interceptors: [LoggingInterceptor()]);
+final Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+    requestTimeout: Duration(seconds: 5),
+    );
 
 const String baseUrl = 'http://192.168.25.13:8080/transactions';
 
--- bytebank2/lib/screens/transaction_form.dart
+++ bytebank2/lib/screens/transaction_form.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:bytebank2/components/response_dialog.dart';
@@ -89,21 +90,17 @@ class _TransactionFormState extends State<TransactionForm> {
   }
 
   void _save(
-    Transaction transactionCreated,
-    String password,
-    BuildContext context,
-  )async {
-    final Transaction transaction =
-    await _webClient.save(transactionCreated, password).catchError((e) {
-      showDialog(
-          context: context,
-          builder: (contextDialog) {
-        return FailureDialog(e.message);
-      });
-    }, test: (e) => e is Exception);
+      Transaction transactionCreated,
+      String password,
+    BuildContext context) async {
+    Transaction transaction = await _send(transactionCreated, password, context);
 
+     _showSuccessfulMessage(transaction, context);
 
-  if (transaction != null) {
+  }
+
+  Future _showSuccessfulMessage(Transaction transaction, BuildContext context) async {
+    if (transaction != null) {
       await showDialog(
           context: context,
           builder: (contextDialog){
@@ -112,6 +109,25 @@ class _TransactionFormState extends State<TransactionForm> {
       Navigator.pop(context);
 
     }
+  }
 
+  Future<Transaction> _send(Transaction transactionCreated, String password, BuildContext context) async {
+    final Transaction transaction =
+    await _webClient.save(transactionCreated, password).catchError((e) {
+      _showFailureMessage(context, message: 'timeout submitting the transaction');
+    }, test: (e) => e is TimeoutException).catchError((e) {
+      _showFailureMessage(context, message: e.message);
+    }, test: (e) => e is HttpException).catchError((e) {
+      _showFailureMessage(context);
+    });
+    return transaction;
+  }
+
+  void _showFailureMessage(BuildContext context, {String message = 'Unknown error'}) {
+     showDialog(
+        context: context,
+        builder: (contextDialog) {
+          return FailureDialog(message);
+        });
   }
 }
